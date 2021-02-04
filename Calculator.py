8486a225214ed784b40c26395762287daf9c5b0f
Raj Kumar <72345468+DevopsMkt@users.noreply.github.com>
2021-02-04 16:32:11 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,26 +1,48 @@
-import pytest
-import csv
-import os
-import pandas as pd
-from pandas import ExcelWriter
-from pandas import ExcelFile
-import xlrd
-from Calculator import Calculator
+# Program make a simple calculator that can add, subtract, multiply and divide using functions
 
-def prepareData(operator):
-  df = pd.read_excel('./testdata/TestData.xlsx', sheet_name = operator)
-  thisList = []
-  for i in df.index:
-    temp = (df['Operand1'][i], df['Operand2'][i], df['Result'][i])
-    thisList.append(temp)  
-  return thisList
+# define functions
+def add(x, y):
+   """This function adds two numbers"""
 
-@pytest.mark.parametrize("operand1, operand2, expectedResult", prepareData('Add'))
-def test_add(operand1, operand2, expectedResult):
-  cal = Calculator()
-  assert cal.add(operand1, operand2) == expectedResult
+   return x + y
 
-@pytest.mark.parametrize("operand1, operand2, expectedResult", prepareData('Subtract'))
-def test_subtract(operand1, operand2, expectedResult):
-  cal = Calculator()
-  assert cal.subtract(operand1, operand2) == expectedResult
+def subtract(x, y):
+   """This function subtracts two numbers"""
+
+   return x - y
+
+def multiply(x, y):
+   """This function multiplies two numbers"""
+
+   return x * y
+
+def divide(x, y):
+   """This function divides two numbers"""
+
+   return x / y
+
+# take input from the user
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
